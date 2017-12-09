04204d790a47b16d8914248ef4172613118bf89b
mdymek <mdymek@pgs-soft.com>
2017-12-09 14:24:18 +0100

    Initial commit

---
+++ src/test/java/GetRestTest.java
@@ -0,0 +1,40 @@
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsString;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class GetRestTest extends FunctionalTest {
+    @Test
+    public void basicPingTest() {
+        given().when().get("/posts").then().statusCode(200);
+    }
+
+    @Test
+    public void verifyPostName() {
+        given().when().get("/posts/1").then()
+                .body(containsString("sunt aut facere"));
+    }
+
+    @Test
+    public void verifyExactTitle() {
+        given().when().get("/posts/2").then()
+                .body("title", equalTo("qui est esse"));
+    }
+
+    @Test
+    public void verifyUsersCity() {
+        given().when().get("/users/1").then()
+                .body("address.city", equalTo("Gwenborough"));
+    }
+
+    @Test
+    public void verifyUsingChaining() {
+        given().when().get("users/2").then()
+                .body("name", equalTo("Ervin Howell"))
+                .body("address.zipcode", equalTo("90566-7771"))
+                .body("address.geo.lat", equalTo("-43.9509"))
+                .statusCode(200);
+    }
+}
---
+++ src/test/java/PostRestTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.jayway.restassured.RestAssured.given;
+
+public class PostRestTest extends FunctionalTest {
+    @Test
+    public void AddPost() {
+        Map<String, String> post = new HashMap<String, String>();
+        post.put("title", "Test RestAssured post");
+        post.put("body", "RestAssured post body test content");
+        post.put("userId", "7");
+
+        given()
+                .contentType("application/json")
+                .body(post)
+                .when().post("/posts")
+                .then().statusCode(201);
+    }
+}
