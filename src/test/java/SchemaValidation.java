2216753eb5d2780fe9ef87ea1da0ebf39fa0197e
mdymek <mdymek@pgs-soft.com>
2017-12-13 12:15:37 +0100

    Add user schema, add validation case, add maven dependency

---
+++ src/test/java/SchemaValidation.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class SchemaValidation extends FunctionalTest {
+    @Test
+    public void userSchemaValidation() {
+        given().when().get("/users").then().assertThat().body(matchesJsonSchemaInClasspath("user-schema.json"));
+    }
+}
