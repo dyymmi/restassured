82d3901a16653d1c3f305c8193192e9f2f1a9605
mdymek <mdymek@pgs-soft.com>
2017-12-19 14:56:14 +0100

    Refactor and optimize imports

--- src/test/java/GetRestTest.java
+++ src/test/java/GetRestTest.java
@@ -2,10 +2,9 @@ import org.junit.Test;
 
 import java.util.Random;
 
+import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.Matchers.containsString;
-
-import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class GetRestTest extends FunctionalTest {
